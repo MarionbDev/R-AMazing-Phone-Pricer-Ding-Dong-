95f7e36bd20c1f30d11b9082f9ec9903229b654f
Mickaël Silmain <quentin1813@yahoo.fr>
2023-06-28 16:14:47 +0200

    Frontend of login finished, backend with database need to be implemented

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,30 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
 
 import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <p>coucou</p>
-    </div>
+    <main>
+      <span id="background-wrap">
+        <div className="bubble x1" />
+        <div className="bubble x2" />
+        <div className="bubble x3" />
+        <div className="bubble x4" />
+        <div className="bubble x5" />
+        <div className="bubble x6" />
+        <div className="bubble x7" />
+        <div className="bubble x8" />
+        <div className="bubble x9" />
+        <div className="bubble x10" />
+      </span>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </main>
   );
 }
 
