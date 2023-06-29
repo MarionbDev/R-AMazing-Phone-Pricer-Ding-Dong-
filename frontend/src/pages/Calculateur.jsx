c9f371ec942babcd3abad0941aa57cce009c25c8
Mickaël Silmain <quentin1813@yahoo.fr>
2023-06-29 06:44:13 +0200

    added faq, calculateur pages and header

---
+++ frontend/src/pages/Calculateur.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable import/no-duplicates */
+import React from "react";
+import style from "./Calculateur.module.scss";
+
+function Calculateur() {
+  return (
+    <div className={style.container}>
+      <h4>Calculateur</h4>
+      <h5>Calcule, fils d'unijambiste !</h5>
+    </div>
+  );
+}
+
+export default Calculateur;
